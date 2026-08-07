85af782012a6f54c0c6324dfe0fb117b0062a00f
vimauryaxx <vikash94.maurya@routerarchitects.com>
2026-08-07 11:47:42 +0530

    feature : implement Websocket Client & JSON-RPC handler

--- pkg/websocket/client.go
+++ pkg/websocket/client.go
@@ -63,14 +63,14 @@ const (
 )
 
 const (
-	defaultCompressionThresholdBytes = 2048
-	defaultMaxFrameSize              = 11 * 1024 * 1024
-	defaultConnectTimeout            = 10 * time.Second
-	defaultWriteTimeout              = 10 * time.Second
-	defaultPongTimeout               = 60 * time.Second
-	defaultPingInterval              = 30 * time.Second
-	defaultInitialBackoff            = 2 * time.Second
-	defaultMaxBackoff                = 60 * time.Second
+	defaultCompressionThresholdBytes     = 2048
+	defaultMaxFrameSize                  = 11 * 1024 * 1024
+	defaultConnectTimeout                = 10 * time.Second
+	defaultWriteTimeout                  = 10 * time.Second
+	defaultPongTimeout                   = 60 * time.Second
+	defaultPingInterval                  = 30 * time.Second
+	defaultInitialBackoff                = 2 * time.Second
+	defaultMaxBackoff                    = 60 * time.Second
 	defaultStableSessionThresholdSeconds = 60 * time.Second
 )
 

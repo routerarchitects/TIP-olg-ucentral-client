4ea817a36f696e4eb3e699571b2b55ec4cf0558a
vimauryaxx <vikash94.maurya@routerarchitects.com>
2026-08-24 10:29:22 +0530

    feature : implement initialization and wiring

--- cmd/ucentral-client/main_test.go
+++ cmd/ucentral-client/main_test.go
@@ -91,7 +91,7 @@ func TestHandleNATSResultOverflow_UpgradePersistenceFailure(t *testing.T) {
 	cacheTTL := reqmgr.CacheTTLConfig{}
 	scheduler := queues.NewPriorityScheduler(10, 10)
 	store := &errorMockStore{}
-	
+
 	m, _ := reqmgr.NewRequestManager(10*time.Second, cacheTTL, cache, scheduler, store, 1000, 15*time.Minute, 100)
 
 	components := &AppComponents{
